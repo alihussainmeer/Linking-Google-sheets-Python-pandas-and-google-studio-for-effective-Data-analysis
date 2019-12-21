78c6f82dc6104e77206b67ca230fa96b45159af3
Ali Hussain Mir <alihussainmeer@gmail.com>
2019-12-21 14:52:35 +0400

    Update Program.py

--- Program.py
+++ Program.py
@@ -1,19 +1,19 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-
 import gspread
 import gspread_dataframe as gd
 from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+
+df = pd.DataFrame()
 
 scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive"]
 
-credentials = ServiceAccountCredentials.from_json_keyfile_name("/home/future/Downloads/spreadsheetexample.json", scope) #replace spreadsheetexample.json with actual json file which you downloaded
-
+#replace spreadsheetexample.json with actual json file which you downloaded
+#the link for setting up and getting the json file is provided in the readme
+credentials = ServiceAccountCredentials.from_json_keyfile_name("your_file_path_here", scope) 
 gc = gspread.authorize(credentials)
-ws = gc.open("Test").sheet1 #change "you can change it to desired worksheet name by replacing sheet1
+
+#replace the name in between the gc.open parenthesis  with the name of your spreadsheet
+ws = gc.open("name_of_your_google_spreadsheet").sheet1 #change "you can change it to desired worksheet name by replacing sheet1
 
 #gspread
 #existing = gd.get_as_dataframe(ws)
@@ -26,3 +26,4 @@ if choice in "Y":
 else:
     print("Restart")
 
+df
